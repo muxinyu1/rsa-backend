eccf461cdff8ca4dd69ef132d2eec11429c3f2a5
Xinyu Mu <mxy2233@outlook.com>
2024-11-02 17:16:17 +0800

    添加从环境变量读取端口

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use rocket::{launch, routes};
+use std::env;
+
 use backend::routes::*;
+use rocket::{figment::value, launch, routes};
 
 mod algorithms;
+mod backend;
 mod bigint;
 mod rsa;
-mod backend;
 
 #[launch]
 fn rocket() -> _ {
+    let port: u16 = match env::var("RUST_API_PORT") {
+        Ok(value) => {
+            if let Ok(value) = u16::from_str_radix(&value, 10) {
+                value
+            } else {
+                8080
+            }
+        },
+        Err(_) => 8080,
+    };
     rocket::build()
         .mount("/", routes![key_gen, encrypt, decrypt, sign, verify_sign])
         .configure(rocket::Config {
-            port: 8080,
+            port,
             address: "127.0.0.1".parse().unwrap(),
             ..rocket::Config::default()
         })
